e000a6402cb03e94cde465daa4358934e1f1d122
Andre <andrehifran@gmail.com>
2025-07-09 12:51:16 -0400

    Corrige gráfico de pizza e refina layout Power BI

--- dashboards_powerbi33.py
+++ dashboards_powerbi33.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from utils.mapa import mostrar_mapa
+import numpy as np
+import matplotlib.pyplot as plt # type: ignore
+# 
 
 st.set_page_config(page_title="Dashboard Diabetes", layout="wide")
 
